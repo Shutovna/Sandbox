49b1eff244892ea821d690f082efd3b2e94884b1
Nikita Shutov <ShutovNA1987@gmail.com>
2024-07-10 00:12:52 +0700

    Second commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category ='toys';
+
+console.log(`https://localhost:8080/${category}/123`)
+
+const user = 'Ivan';
+console.log(`preved ${user}`)
+console.log(`preved ${user}`)
