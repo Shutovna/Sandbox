fc5578d3999c315448a76c5cc6cb0115c526ff6f
Nikitos <shutovna1987@gmail.com>
2024-07-16 20:41:03 +0700

    Function constructor example

---
+++ js/!function_constructor.js
@@ -0,0 +1,21 @@
+"use strict"
+
+function Person(id, name, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.referal = false;
+}
+
+const p1 = new Person(1, "Ivan", "Ivan@gmail.com")
+const p2 = new Person(2, "Alex", "Alex@gmail.com")
+
+Person.prototype.save = function () {
+    console.log(`Saving ${this.name}`);
+}
+
+console.log(p1)
+console.log(p2)
+
+p1.save()
+p2.save()
